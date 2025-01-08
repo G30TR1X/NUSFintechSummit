8509627b690b8429a1507effe1421dfb619a3bfd
Vane <88143635+evan052006@users.noreply.github.com>
2025-01-08 22:28:52 +0700

    ability to turn manual move on/off

--- Scripts/player_preview.gd
+++ Scripts/player_preview.gd
@@ -5,12 +5,9 @@ extends CharacterBody2D
 const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 
+@export var manual_move_state = false
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
+func manual_move() -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -36,4 +33,15 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	if manual_move_state == 0:
+		manual_move()
+	
 	move_and_slide()
